05341a4bd06a19587db15074a1a8ae1d0249a623
pbraza07 <pbraza@gmail.com>
2026-09-03 16:24:02 -0400

    de

--- app/api/staff/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { COOKIE_NAME } from "@/lib/staff-auth";
-
-export async function GET() {
-  (await cookies()).delete(COOKIE_NAME);
-  redirect("/");
-}
